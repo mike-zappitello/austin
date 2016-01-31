1027d74961b1c4ac615338fd156aed8f5a951e81
mzappitello <mzappitello@gmail.com>
2016-01-31 15:50:18 +0000

    setup main to actually do the reading

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-Slack = require 'slack-client'
+Slack = require('slack-client');
 
-slackToken = ''
-autoReconnect = true
-autoMark = true
+slackToken = 'xoxb-19918014225-2YhCXhSDmq2mB75tnSfxlCRP';
+autoReconnect = true;
+autoMark = true;
 
-slack = new Slack(slackToken, autoReconnect, autoMark)
+slack = new Slack(slackToken, autoReconnect, autoMark);
 
-slack.on 'open', ->
-  console.log "Connected to #{slack.team.name} as @#{slack.self.name}"
+slack.on('open', function(data) {
+  console.log("open\n" + data);
+});
 
-slack.on 'message', (message) ->
-  console.log message
+slack.on('message', function(message) {
+  console.log("message\n" + message);
+});
 
-slack.on 'error', (error) ->
-  console.log "Error", err
+slack.on('error', function(error) {
+  console.log("error\n" + error);
+});
 
-slack.login()
+slack.login();
