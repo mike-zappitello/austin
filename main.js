e1cdc9cd79e2a3129c8bb115030f847cedab04dd
mzappitello <mzappitello@gmail.com>
2016-02-01 07:34:07 +0000

    schedule daily games task on startup
    * add scheduler to austin, schedule posting games in austin_testing
    * getTodaysGames now actually uses todays date

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
     austin = require('./lib/austin.js');
 
+// get the configuration from the config file
 var configuration = JSON.parse(fs.readFileSync('configuration.json'));
 
+// spawn an austin carr bot
 var austin_carr = austin.Austin(configuration);
-
