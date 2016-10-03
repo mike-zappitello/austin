db3d369b0f10686bb8bdb99cc886446752035271
mzappitello <mzappitello@gmail.com>
2016-10-03 01:54:36 +0000

    add winston logging service
    * main adds the logging dir to the configuration
    * lib/austing creates the logging interest and passses it to the slack clients
    * package.json and gitignore changed to add winston and ignore the logs dir.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var fs = require('fs'),
-    austin = require('./lib/austin.js');
+var fs      = require('fs'),
+    austin  = require('./lib/austin.js'),
+    path    = require('path');
 
 // get the configuration from the config file
 var configuration = JSON.parse(fs.readFileSync('configuration.json'));
+
+configuration.loggingDir = path.resolve(__dirname, 'logs');
 
 // spawn an austin carr bot
 var austin_carr = austin.Austin(configuration);
