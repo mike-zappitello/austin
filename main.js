adfda94cbef2e6c261c1fdca5a74e6a8973bbcc4
mzappitello <mzappitello@gmail.com>
2016-10-28 17:17:46 +0000

    improve configuration loading.
    lib/configLoader.js does:
      * check for config.name
      * settup loggingDir
      * check for slack token

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-var fs      = require('fs'),
-    austin  = require('./lib/austin.js'),
-    path    = require('path');
+var austin     = require('./lib/austin.js'),
+    loadConfig = require('./lib/configLoader').loadConfig;
 
 var args = process.argv.slice(2);
 
 // get the configuration from the config file
-var configuration = JSON.parse(fs.readFileSync(args[0]));
-
-configuration.loggingDir = path.resolve(__dirname, 'logs');
+var configuration = loadConfig(args[0]);
 
 // spawn an austin carr bot
 var austin_carr = austin.Austin(configuration);
