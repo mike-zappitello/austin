cc2285edf63b3c310d004208f5663f4439840518
mzappitello <mzappitello@gmail.com>
2016-10-28 17:01:06 +0000

    main dot js now takes a config arg on the cli
    * do an arg parse in main.js
    * create a logging file based on config name

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var fs      = require('fs'),
     austin  = require('./lib/austin.js'),
     path    = require('path');
 
+var args = process.argv.slice(2);
+
 // get the configuration from the config file
-var configuration = JSON.parse(fs.readFileSync('configuration.json'));
+var configuration = JSON.parse(fs.readFileSync(args[0]));
 
 configuration.loggingDir = path.resolve(__dirname, 'logs');
 
