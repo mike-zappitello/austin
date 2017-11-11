a1af0b5f5a2e96528e11a537790130da9ee3e4ec
mzappitello <mzappitello@gmail.com>
2017-11-11 14:25:27 +0000

    initial setup
    * now we're using botkit, which handles most of the boilerplate i was poorly
      implimenting.
    * updated node on my machine and syntax in general.
    * removed context and message parser which are now unneeded.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var austin     = require('./lib/austin.js'),
-    loadConfig = require('./lib/configLoader').loadConfig;
+"use strict";
 
-var args = process.argv.slice(2);
+const austin     = require('./lib/austin.js');
+const loadConfig = require('./lib/configLoader').loadConfig;
+
+const args = process.argv.slice(2);
 
 // get the configuration from the config file
-// TODO(mike) - this should also be creating the resources and be moved to the
-// constructor of austin.
-var configuration = loadConfig(args[0]);
+const configuration = loadConfig(args[0]);
 
 // spawn an austin carr bot
-var austin_carr = austin.Austin(configuration);
+const austin_carr = austin.Austin(configuration);
